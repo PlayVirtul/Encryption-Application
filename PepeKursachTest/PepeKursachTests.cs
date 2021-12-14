bfe84bf122d55eaecf59217c1e2f243a40b86113
PlayVirtul <playvirtul@gmail.com>
2021-12-14 12:48:20 +0300

    Добавление тестов

--- PepeKursachTest/PepeKursachTests.cs
+++ PepeKursachTest/PepeKursachTests.cs
@@ -8,7 +8,7 @@ namespace PepeKursachTest
     public class PepeKursachTests
     {
         [TestMethod]
-        public void EncodeExampleTextReturned()
+        public void EncodeExample1TextReturned()
         {
             string text = "Привет, это unit тест для зашифровки!";
             string key = "тест";
@@ -21,11 +21,37 @@ namespace PepeKursachTest
         }
 
         [TestMethod]
-        public void DecodeExampleTextReturned()
+        public void EncodeExample2TextReturned()
+        {
+            string text = "This text should not change )0)0))";
+            string key = "тест";
+            string expected = "This text should not change )0)0))";
+
+            Coderator coderator = new Coderator();
+            string actual = coderator.Encode(text, key);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecodeExample1TextReturned()
         {
             string text = "Unit ейге ёцбчкта вгуыцчт!";
             string key = "Тест";
             string expected = "Unit тест успешно пройден!";
+
+            Coderator coderator = new Coderator();
+            string actual = coderator.Decode(text, key);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecodeExample2TextReturned()
+        {
+            string text = "This text should not change )0)0))";
+            string key = "Тест";
+            string expected = "This text should not change )0)0))";
 
             Coderator coderator = new Coderator();
             string actual = coderator.Decode(text, key);
